d2bac09f6558378b918c2d1b14e91b6d4244ab75
cty0830 <s011090@cnsh.mlc.edu.tw>
2026-03-16 18:50:24 +0800

    f

--- src/main/java/com/example/demo/controller/apicontroller.java
+++ src/main/java/com/example/demo/controller/apicontroller.java
@@ -1,5 +1,11 @@
 package com.example.demo.controller;
 
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,7 +42,6 @@ public class apicontroller{
 	 * 結果 1 : hi mary, 0(未成年)
 	 * 限制 : name 為必要參數, age 為可選參數(初始值0)
 	 */
-	
 	@GetMapping(value = "/greet" )
 	public String greet(@RequestParam(value = "name", required = true) String username,
 			            @RequestParam(value = "age", required = false, defaultValue = "0")  Integer userage) {
@@ -48,7 +53,6 @@ public class apicontroller{
 	 * 3. 上述的精簡
 	 * 方法參數名和請求參數名同
 	 */
-	
 	@GetMapping(value = "/greet2" )
 	public String greet2(@RequestParam String name,
 			            @RequestParam(required = false, defaultValue = "0")  Integer age) {
@@ -64,7 +68,6 @@ public class apicontroller{
 	 * 判斷 : bmi <= 18 過輕, bmi > 23 過重
 	 * 結果 : 身高:170cm 體重:60kg bmi = 20.76(正常)
 	 */
-	
 	@GetMapping(value = "/bmi" )
 	public String bmi(@RequestParam(required = false, defaultValue = "0") Double h,
                       @RequestParam(required = false, defaultValue = "0") Double w) {
@@ -84,7 +87,7 @@ public class apicontroller{
 	    return String.format("身高:%.0fcm 體重:%.0fkg BMI:%.2f(%s)", h, w, bmi, status);
 	}
 	
-	/** 4. 回傳 json 結構
+	/** 5. 回傳 json 結構
 	 * 路徑 : /json/bmi?h=170$w=60
 	 * 網址 : http://localhost:8080/api/json/bmi?h=170&w=60
 	 * 判斷 : bmi <= 18 過輕, bmi > 23 過重
@@ -99,8 +102,7 @@ public class apicontroller{
 	 * 		} 
 	 *}
 	 */
-	
-	@GetMapping(value = "/json/bmi")
+	@GetMapping(value = "/json/bmi", produces = "application/json;charset = utf-8")
 	public ResponseEntity<responseapi<bmi>> calcbmi(@RequestParam(required = false) Double h,
             										   @RequestParam(required = false) Double w) {
 		
@@ -120,5 +122,49 @@ public class apicontroller{
 		// ok => http 200
 		//return ResponseEntity.ok(new responseapi<bmi>("計算成功", b));
 		return ResponseEntity.ok(responseapi.success("計算成功", b));
+	}
+	
+	/** 6. 同名多筆資料
+	 * 路徑 : /json/age?age=17&age=20&age=21
+	 * 網址 : http://localhost:8080/api/json/age?age=17&age=20&age=21
+	 * 請判斷平均年齡
+	 */
+	@GetMapping(value = "/json/age", produces = "application/json;charset = utf-8")
+	public ResponseEntity<responseapi<Object>> getaverage(@RequestParam(name = "age", required = false)List<Integer> ages) {
+		if(ages == null || ages.size() == 0) {
+			return ResponseEntity.badRequest().body(responseapi.error("請輸入年齡(age)"));
+		}
+		double avg = ages.stream().mapToInt(Integer::valueOf).average().orElseGet(() -> 0);
+		Object data = Map.of("年齡", ages, "平均年齡", String.format("%.1f", avg));
+		return ResponseEntity.ok(responseapi.success("計算成功", data));
+	}
+
+	/** 7. lab practice: 得到多筆 score 資料
+	 * 路徑 : /json/score?score=80&score=100&score=50&score=70&score=30
+	 * 網址 : http://localhost:8080/api/json/score?score=80&score=100&score=50&score=70&score=30
+	 * 請設計一個方法能判斷出: 最高分 最低分 平均 總分 及格分數 不及格分數
+	 */
+	@GetMapping(value = "/json/score", produces = "application/json;charset = utf-8")
+	public ResponseEntity<responseapi<Object>> getscore(@RequestParam(name = "score", required = false)List<Integer> scores) {
+		if(scores == null || scores.size() == 0) {
+			return ResponseEntity.badRequest().body(responseapi.error("請輸入成績(scores)"));
+		}
+		//統計資料
+		IntSummaryStatistics stat = scores.stream().mapToInt(Integer::valueOf).summaryStatistics(); 
+		
+		//利用 collectors.partitioningby 分組, key = true 及格, 反之不及格
+		Map<Boolean, List<Integer>> resultMap = scores.stream()
+				.collect(Collectors.partitioningBy(score -> score >= 60));
+		
+		Object data = Map.of(
+				"最高分: ", stat.getMax(),
+				"最低分: ", stat.getMin(),
+				"平均成績: ", stat.getAverage(),
+				"總分: ", stat.getSum(),
+				"及格: ", resultMap.get(true),
+				"不及格: ", resultMap.get(false)
+				);
+		
+		return ResponseEntity.ok(responseapi.success("計算成功", data));
 	}
 }
